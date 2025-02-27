a2bcbabe4815e940f99ed4e46b9d36cdb112bd3c
fegan104 <fegan104@gmail.com>
2025-02-26 21:30:26 -0500

    you can add and re-order items

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -1,9 +1,68 @@
 "use client"; // Ensures this runs only on the client side
 import { useEffect, useState } from "react";
+import {
+  DndContext,
+  closestCenter,
+  useSensor,
+  useSensors,
+  PointerSensor,
+  KeyboardSensor,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
+const initialItems = ["SPD", "Z", "NSDAP", "DVU", "KPD", "DNVP"];
+
+function SortableItem({ id }: { id: string }) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    color: "black",
+  };
+
+  return (
+    <li
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="p-2 bg-gray-200 rounded-lg cursor-grab"
+    >
+      {id}
+    </li>
+  );
+}
 
 export default function Vote() {
   const [electionId, setElectionId] = useState<string | null>(null);
+  const [items, setItems] = useState<string[]>([]);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over || active.id === over.id) return;
+
+    if (active.id !== over?.id) {
+      const oldIndex = items.indexOf(active.id as string);
+      const newIndex = items.indexOf(over?.id as string);
+      setItems(arrayMove(items, oldIndex, newIndex));
+    }
+  };
 
   useEffect(() => {
     const parsedId = window.location.hash.slice(2);//remove leading "#/"
@@ -16,8 +75,24 @@ export default function Vote() {
     <div className="grid items-center justify-items-center">
       <main className="flex flex-col items-left">
 
+        <ul>
+          {initialItems.map((item, index) => (
+            <li onClick={() => setItems([...items, item])} key={index}>{item}</li>
+          ))}
+        </ul>
+
+        <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+          <SortableContext items={items} strategy={verticalListSortingStrategy}>
+            <ul className="space-y-2 p-4 border rounded-lg">
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </ul>
+          </SortableContext>
+        </DndContext>
+
         <button onClick={() => console.log("Voting! in " + electionId)}>
-          Save
+          Cast Vote!
         </button>
       </main>
     </div>
